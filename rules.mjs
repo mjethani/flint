63e732712855f865a14e5fb0dbd5a934bb97dcca
Manish Jethani <code@manishjethani.io>
2021-08-22 05:03:38 +0530

    Use single quotes in rules.mjs

--- rules.mjs
+++ rules.mjs
@@ -17,14 +17,14 @@
 
 export default [
   {
-    pattern: "^\\s+",
-    type: "warning",
-    message: "Leading whitespace"
+    pattern: '^\\s+',
+    type: 'warning',
+    message: 'Leading whitespace'
   },
   {
     // Trailing whitespace is OK in comments.
-    pattern: "^\\s*[^!].*\\s+$",
-    type: "warning",
-    message: "Trailing whitespace"
+    pattern: '^\\s*[^!].*\\s+$',
+    type: 'warning',
+    message: 'Trailing whitespace'
   },
 ];
